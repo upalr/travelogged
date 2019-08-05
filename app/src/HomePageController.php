de84e97b5b5db658e21012b5179713cb8ea06177
upalr <shovon.0323@gmail.com>
2019-08-05 15:24:20 +1000

    Implemented latest article options using the SS ORM.

--- app/src/HomePageController.php
+++ app/src/HomePageController.php
@@ -4,5 +4,10 @@
 use SilverStripe\CMS\Controllers\ContentController;
 class HomePageController extends PageController
 {
-
+    public function LatestArticles($count = 3)
+    {
+        return ArticlePage::get()
+            ->sort('Created', 'DESC')
+            ->limit($count);
+    }
 }
